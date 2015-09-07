898d48bd65ba9b72efb4e4935fb7d1add1214b43
LarsBroeders <lars_broeders@hotmail.com>
2015-09-07 21:26:24 +0200

    Write out first two chapters of FF#

---
+++ Lars Broeders 0861065/FriendlyFSharp/FriendlyFSharp/Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    do Chapter1.BallSimulation.simulation()
+    do Chapter2.RocketSimulation.simulation()
+    0 // return an integer exit code
