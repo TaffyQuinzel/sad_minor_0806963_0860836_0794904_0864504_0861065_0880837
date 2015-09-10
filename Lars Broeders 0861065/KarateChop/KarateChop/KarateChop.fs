204edc64fbf225288652ad39c0e852971a8c0ed8
LarsBroeders <lars_broeders@hotmail.com>
2015-09-10 18:25:21 +0200

    Write chop exercise

---
+++ Lars Broeders 0861065/KarateChop/KarateChop/KarateChop.fs
@@ -0,0 +1,41 @@
+﻿module KarateChop
+  type IntList = 
+    | Cons of int*IntList
+    | Empty
+    
+    static member (++) (x: int, xs: IntList): IntList =
+      Cons(x, xs)
+
+    member this.Get(index: int): int =
+      let rec get (index: int, list: IntList): int =
+        match list with
+        | Cons(x, xs) ->
+          if index = 0 then
+            x
+          else
+            get(index - 1, xs)
+        | Empty -> failwith "Index out of bounds"
+      get (index, this)
+
+      member this.Length: int =
+        let rec length(l: IntList) =
+          match l with
+          | Empty -> 0
+          | Cons(x, xs) -> 1 + length xs
+        length this
+
+    static member chop(i: int, l: IntList): int =
+      let rec chop (key: int, list: IntList, min: int, max: int) =
+        if(max < min) then
+          -1
+        else
+          let mid = ((max - min) / 2) + min
+          let key_at_mid = list.Get(mid)
+          if key_at_mid < key then 
+            chop(key, list, mid + 1, max)
+          elif key_at_mid > key then 
+            chop(key, list, min, mid - 1)
+          else 
+            mid
+      chop(i, l, 0, l.Length - 1)
+      
---
+++ Lars Broeders 0861065/KarateChop/KarateChop/KarateChopTest.fs
@@ -0,0 +1,41 @@
+﻿module KarateIntList.chopTest
+  open NUnit.Framework
+  open FsUnit
+  open KarateChop
+
+  [<TestFixture>]
+  type GetTest() =
+    [<Test>]
+    member this.GetEmptyList() = 
+      let list = Empty
+      (fun () -> list.Get(2) |> ignore) |> should throw typeof<System.Exception>
+
+    [<Test>]
+    member this.GetFirstElement() =
+      let list = Cons(1, Cons(3, Cons(5, Empty)))
+      list.Get(0) |> should equal 1
+
+    [<Test>]
+    member this.GetNotFirstElement() = 
+      let list = Cons(1, Cons(3, Cons(5, Empty)))
+      list.Get(1) |> should equal 3
+      list.Get(2) |> should equal 5
+
+  [<TestFixture>]
+  type ChopTest() =
+    
+    [<Test>]
+    member this.SearchElementInEmptyList() =
+      IntList.chop(3, Empty) |> should equal -1
+
+    [<Test>]
+    member this.SearchExistingElementInNonEmptyList() =
+      let list = Cons(1, Cons(3, Cons(5, Empty)))
+      IntList.chop(1, list) |> should equal 0
+      IntList.chop(3, list) |> should equal 1
+      IntList.chop(5, list) |> should equal 2
+    
+    [<Test>]
+    member this.SearchMissingElementInNonEmptyList() =
+      let list = Cons(1, Cons(3, Cons(5, Empty)))
+      IntList.chop(4, list) |> should equal -1
